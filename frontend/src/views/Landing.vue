29eb70d5092b2a31d30f73bd912252bf44f24132
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:37:43 -0300

    Create Landing.vue

---
+++ frontend/src/views/Landing.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { onMounted, ref } from "vue"
+
+const message = ref("Loading...")
+
+onMounted(() => {
+  message.value = "Welcome to Site Twilight!"
+})
+</script>
+
+<template>
+  <div class="landing text-center mt-10">
+    <h2 class="text-3xl font-bold mb-4">Landing Page</h2>
+    <p class="text-lg">{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+/* opcional */
+</style>
