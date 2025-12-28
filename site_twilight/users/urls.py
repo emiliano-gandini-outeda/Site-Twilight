542a3b057fbfa3c71bc17812e8f4e783638f55b8
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:40:51 -0300

    Update urls.py

--- site_twilight/users/urls.py
+++ site_twilight/users/urls.py
@@ -2,7 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/roblox/", views.roblox_login, name="roblox_login"),
-    path("login/roblox/callback/", views.roblox_callback, name="roblox_callback"),
-    path("logout/", views.logout_view, name="logout"),
+    path("login/roblox/", views.accounts.roblox_login, name="roblox_login"),
+    path("login/roblox/callback/", views.accounts.roblox_callback, name="roblox_callback"),
+    path("logout/", views.accounts.logout_view, name="logout"),
+
 ]
+"""     
+    path("api/staff/moderation/", views.api_moderation_dashboard),
+    path("api/staff/discord/", views.api_discord_moderation_panel),
+
+    path("api/staff/ssu/", views.api_change_ssu_status),
+
+    path("api/rp/doc/<int:doc_id>/", views.api_edit_rp_files_basic),
+    path("api/rp/doc/full/<int:doc_id>/", views.api_edit_rp_files_full),
+
+    path("api/rp/factions/", views.api_faction_moderation_basic),
+    path("api/rp/factions/full/", views.api_faction_moderation_full),
+
+    path("api/rp/actors/", views.api_supervise_actors_basic),
+    path("api/rp/actors/full/", views.api_supervise_actors_full), """
