ca80bfd8cf328d858527545a02a6867909367998
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-27 21:40:02 -0300

    Update settings.py

--- site_twilight/site_twilight/settings.py
+++ site_twilight/site_twilight/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'site_twilight.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "frontend" / "dist"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,11 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "frontend" / "dist" / "assets",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # prod
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
