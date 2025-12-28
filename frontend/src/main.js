97dad275cf27aff4819f5b5a9a995c0fbe71b816
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 17:38:50 -0300

    Easy Header hookup

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
+import Header from './components/Header.vue'
 
 createApp(App)
   .use(router)
   .mount("#app")
+  .component("Header", Header)
