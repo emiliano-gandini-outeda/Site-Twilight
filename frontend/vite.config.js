2d90654e15109501ef398bb2d98e196b42aff882
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:37:47 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path"
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/static/' : './', // /static/ for prod | / for dev
   plugins: [vue()],
   css: {
     transformer: "lightningcss",
@@ -14,5 +15,12 @@ export default defineConfig({
   },
   build: {
     target: "es2020",
+    outDir: 'dist',
+    assetsDir: 'assets', // all CSS/JS will go under dist/assets
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+}))
