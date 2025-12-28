9f833aff256fee59a26da44e8fca1fc86af09dd3
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 12:33:24 -0300

    Update settings.py

--- site_twilight/site_twilight/settings.py
+++ site_twilight/site_twilight/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t7m-&&4$ofey9_w2%ike&@%i4t3e(haum^xcm4joxu-lis*sls'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
