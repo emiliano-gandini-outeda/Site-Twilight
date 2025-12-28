35c1ce99d12ace0641b140628c8568f0b116e43d
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:37:38 -0300

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
+import Landing from "@/views/Landing.vue"
+import Privacy from "@/views/Privacy.vue"
+import TermsOfService from "@/views/TermsOfService.vue"
 
 const routes = [
-  {
-    path: "/privacy",
-    component: () => import("../views/Privacy.vue"),
-  },
-  {
-    path: "/terms-of-service",
-    component: () => import("../views/TermsOfService.vue"),
-  },
+  { path: "/", name: "Landing", component: Landing },
+  { path: "/privacy", name: "Privacy", component: Privacy },
+  { path: "/terms-of-service", name: "TermsOfService", component: TermsOfService },
 ]
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+export default router
