e6b8864f4d62d4271dc0fe5e1f8e249b1b4dafb7
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:40:44 -0300

    Update models.py

--- site_twilight/users/models.py
+++ site_twilight/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .permissions import STAFF_PERMISSIONS
 
 # Create your models here.
 
@@ -15,5 +16,74 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
     USERNAME_FIELD = "id"
 
+    first_login = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+
+    # Not RP related, only Django Backend
+    is_staff = models.BooleanField(default=False)      # Django admin
+    is_superuser = models.BooleanField(default=False)  # Django super admin
+
+    def has_permission(self, permission: str) -> bool:
+        for role in self.staff_roles.all():
+            perms_by_level = STAFF_PERMISSIONS.get(role.scope, {})
+            for lvl in range(1, role.level + 1):
+                if permission in perms_by_level.get(lvl, set()):
+                    return True
+        return False
+
     def __str__(self):
         return f'Usuario {self.roblox_username} #{self.roblox_id}'
+
+class StaffRole(models.Model):
+    class Scope(models.TextChoices):
+        GLOBAL = "global", "Moderation Lead"
+        INGAME = "ingame", "In-Game Moderation"
+        DISCORD = "discord", "Discord Moderation"
+        ROLEPLAY = "roleplay", "Roleplay Moderation"
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="staff_roles",
+    )
+
+    scope = models.CharField(
+        max_length=20,
+        choices=Scope.choices,
+    )
+
+    level = models.PositiveSmallIntegerField(
+        help_text="Higher level = more permissions"
+    )
+
+    assigned_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "scope")
+
+    def __str__(self):
+        return f"{self.user} | {self.scope} L{self.level}"
+
+class Warn(models.Model):
+    target = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="warns"
+    )
+
+    created_by = models.ForeignKey(
+        "users.User",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="issued_warns"
+    )
+
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # opcional
+    expires_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Warn -> {self.target.roblox_username}"
