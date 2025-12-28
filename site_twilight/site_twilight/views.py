901357bf11c9e3c5e356e1d25011ea8476935a2e
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-28 11:40:36 -0300

    Update views.py

--- site_twilight/site_twilight/views.py
+++ site_twilight/site_twilight/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import SiteState
 
 def landingPage(request):
     return render(request, "index.html")
+
+def api_get_ssu_status(request):
+    return JsonResponse({
+        "ssu_status": SiteState.get().ssu_status
+    })
