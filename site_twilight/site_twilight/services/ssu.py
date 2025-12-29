bbcb75a3bad32646ffe3a79a5f408e8f730217a4
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2025-12-29 00:30:07 -0300

    Core Transfer deletions

--- site_twilight/site_twilight/services/ssu.py
+++
@@ -1,7 +0,0 @@
-from ..models import SiteState
-
-def toggle_ssu():
-    state = SiteState.get()
-    state.ssu_status = not state.ssu_status
-    state.save(update_fields=["ssu_status", "updated_at"])
-    return state.ssu_status
