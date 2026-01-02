923cf031a07b730d38afba8d9a38bae41d53a22a
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-01-02 15:03:06 -0300

    Update urls.py

--- site_twilight/users/urls.py
+++ site_twilight/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from .views import accounts, moderation, permissions
 
 urlpatterns = [
     path("login/roblox/", accounts.roblox_login, name="roblox_login"),
     path("login/roblox/callback/", accounts.roblox_callback, name="roblox_callback"),
+    path('login/roblox/callback', 
+         RedirectView.as_view(url='/accounts/login/roblox/callback/', permanent=True)),
     path("logout/", accounts.logout_view, name="logout"),
     
     # Dashboard
