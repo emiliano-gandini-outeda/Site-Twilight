5f5172103c474161eb3bcd66f975404a22441b11
Emiliano Gandini Outeda <emiliano.outeda@gmail.com>
2026-01-01 21:01:53 -0300

    Update urls.py

--- site_twilight/core/urls.py
+++ site_twilight/core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from .api import users
 
 urlpatterns = [
     path("auth/user/", views.api_get_current_user),
     path("ssu/", views.api_get_ssu_status),
+    path("", include("characters.urls")),
+    path("users/<int:roblox_id>/", users.api_get_user_by_roblox_id, name="api_get_user_by_roblox_id"),
+    path("users/<int:roblox_id>/characters/", users.api_get_user_characters, name="api_get_user_characters"),
     path("", include("characters.urls")),
 ]
